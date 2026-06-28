bd6ddcd536c434f301cff64f6acd3bac4d4d1223
Robert Dywane Crosby <robertcrosby@Roberts-MBP.ht.home>
2026-06-28 16:34:21 -0400

    updated project

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// 
+//
+//
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+    .sendForm(
+      'service_27jn1ji',
+      'template_5gay43j',
+      event.target,
+      'zy_XPFxKnKe6Q4mAr'
+    ) .then(() => {
+     loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+    }) .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at treylaker24@gmail.com"
+        );
+    })
+}
